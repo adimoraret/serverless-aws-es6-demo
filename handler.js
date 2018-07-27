2cc2f0ae02d120177bea8e3a1d5eeda8114c9a4d
Adrian Moraret <adrian.moraret@yahoo.com>
2018-07-26 19:57:46 -0700

    implemented demo project

--- handler.js
+++ handler.js
@@ -1,3 +1,11 @@
 export function news(event, context, callback) {
-  rsp.succeed({ response: 'success' }, context, callback)
+
+  const happyDay = () => 'Friday'
+  const buildColor = 'green'
+  const goodNews = [`Today is ${happyDay()}`, `Build is ${buildColor}`]
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(goodNews),
+  })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,5 @@
-const nodeExternals = require('webpack-node-externals')
-
 module.exports = {
-  entry: './handler.js',
+  entry: { handler: './handler.js' },
   target: 'node',
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ["babel-loader"],
-      }
-    ]
-  }
-};
+  mode: 'development'
+}
